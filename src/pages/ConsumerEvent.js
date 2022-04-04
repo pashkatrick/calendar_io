8b68ac390f67c83fd4609283f8475c3a7c436275
Anton Iudin <yudin431@gmail.com>
2022-04-03 20:46:49 -0400

    fetching provider's event types from the server

--- src/pages/ConsumerEvent.js
+++ src/pages/ConsumerEvent.js
@@ -1,26 +1,40 @@
 import '../navigation.css'
 import Calendar from '../components/Calendar'
 import EventForm from '../components/EventForm';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import store from '../redux/store'
 import * as actions from '../redux/actionTypes'
 import { NavLink } from 'react-router-dom';
 
 export default function ConsumerEvent (props) {
 
+    const [types, setTypes] = useState([])
     //have to fetch from the server by loading provider
-    const eventTypes = props.provider.eventTypes
+    const providerId = props.provider._id
+    // const eventTypes = props.provider.eventTypes
+    
+    useEffect (()=> {
+        const axios = require('axios');
+        var config = {
+        method: 'get',
+        url: `http://127.0.0.1:5000/user/${providerId}/types`,
+        headers: { }
+    };    
+        axios(config)
+        .then(function (response) {
+        setTypes(response.data.data)
+    })
+    },[])
     
     return (
             <div className="page">
             <p>Your fency logo</p>
             <h3>{props.link}</h3>
-            <p>You can create an event here</p>
             <h4>{props.provider.name}</h4>
-            <p>has the following type of events</p>
+            {types==null? <p>has the following type of events</p>:<p>this user does not have any events</p>}
             <div className="eventContainer">
-            {eventTypes.map(event=> 
-                <NavLink key={event} className="buttonBright" to={`${event}`}>{event}</NavLink>
+            {types.map(event=> 
+                <NavLink key={event._id} className="buttonBright" to={`${event.title}`}>{event.title}</NavLink>
                 )}
             </div>
     </div>
